63f2484e68497e00b4155d1107030b5ab2339ca9
rfeitosa-maminfo <rodrigo.feitosa@maminfo.com.br>
2026-04-09 17:52:26 -0300

    feat: criação de layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+</head>
+
+<body class="flex flex-col min-h-screen bg-gray-300">
+    <livewire:header />
+
+    <main class="flex-1 bg-gray-100">
+        {{ $slot }}
+    </main>
+
+    <livewire:footer />
+
+    @livewireScripts
+</body>
+
+</html>
