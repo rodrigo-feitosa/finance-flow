d98fdb62351efbaee7fa86b33df9401828f0bf95
rfeitosa-maminfo <rodrigo.feitosa@maminfo.com.br>
2026-04-09 17:47:41 -0300

    feat: criação dos componentes de cabeçalho e rodapé

---
+++ resources/views/components/⚡footer.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<footer class="bg-gray-900 text-gray-400 text-center py-6 mt-10">
+    FinanceFlow © {{ date('Y') }}
+</footer>
---
+++ resources/views/components/⚡header.blade.php
@@ -0,0 +1,67 @@
+<?php
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Http;
+
+new class extends Component
+{
+    public $showMenu = false;
+
+    public function toggleMenu()
+    {
+        $this->showMenu = !$this->showMenu;
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect()->route('index');
+    }
+};
+?>
+
+<header class="bg-gray-900 text-white">
+    <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <a href="/">
+            <h1 class="text-2xl font-bold">FinanceFlow</h1>
+        </a>
+
+        <nav class="space-x-6 relative">
+            <a href="/" class="hover:text-gray-300">Home</a>
+            <a href="/expenses" class="hover:text-gray-300">Despesas</a>
+            <a href="/revenues" class="hover:text-gray-300">Receitas</a>
+            <a href="/cash-flow" class="hover:text-gray-300">Fluxo Financeiro</a>
+            <div class="relative inline-block">
+                <button wire:click="toggleMenu" class="hover:text-gray-300 cursor-pointer">
+                    <i class="fa-solid fa-user"></i>
+                </button>
+
+                @if($showMenu)
+                <div class="absolute right-0 mt-2 w-40 bg-white border rounded-lg shadow-lg">
+                    @auth
+                    <a href="" class="block w-full text-left px-4 py-2 text-black hover:bg-gray-100">
+                        Perfil
+                    </a>
+                    <a href="" class="block w-full text-left px-4 py-2 text-black hover:bg-gray-100">
+                        Configurações
+                    </a>
+
+                    <button wire:click="logout" class="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">
+                        Logout
+                    </button>
+                    @endauth
+
+                    @guest
+                    <a href="/login" class="block w-full text-left px-4 py-2 text-black hover:bg-gray-100">
+                        Login
+                    </a>
+                    <a href="/register" class="block w-full text-left px-4 py-2 text-black hover:bg-gray-100">
+                        Registrar
+                    </a>
+                    @endguest
+                </div>
+                @endif
+            </div>
+        </nav>
+    </div>
+</header>
